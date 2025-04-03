a5410bef72e42c616362f9b7723d1e2b657f616f
sachin-netguru <sachin.kumar@partners-netguru.com>
2025-04-03 15:34:52 +0530

    Added new pages

--- src/Components/SidebarContent/SidebarContent.jsx
+++ src/Components/SidebarContent/SidebarContent.jsx
@@ -16,7 +16,7 @@ const SidebarContent = ({ isSmallScreen, closeSideBarModal }) => {
   return (
     <div className={`flex top-0 left-0 ${isSmallScreen ? 'w-full' : 'w-72'} text-primary h-screen overflow-y-auto bg-[rgb(var(--color-sidebar))] shadow-lg transition-all duration-300`}>
       <div className="flex flex-col items-center py-6 px-4">
-        
+
         {/* Profile Section */}
         <h2 className="text-center text-heading font-extrabold text-2xl mb-3">Sachin Kumar</h2>
         <img
@@ -45,21 +45,23 @@ const SidebarContent = ({ isSmallScreen, closeSideBarModal }) => {
         {/* Social Links */}
         <ul className="mt-6 flex space-x-5 text-blue-400">
           <SocialLink href="https://github.com/ext-sachin-kumar" icon={<FaGithub className="w-5 h-5" />} />
-          <SocialLink href="https://linkedin.com/in/sachinykumar" icon={<FaLinkedin className="w-5 h-5"/>} />
-          <SocialLink href="https://stackoverflow.com/users/6902208/sachin-yadav" icon={<FaStackOverflow className="w-5 h-5"/>} />
+          <SocialLink href="https://linkedin.com/in/sachinykumar" icon={<FaLinkedin className="w-5 h-5" />} />
+          <SocialLink href="https://stackoverflow.com/users/6902208/sachin-yadav" icon={<FaStackOverflow className="w-5 h-5" />} />
         </ul>
 
         {/* Navigation */}
         <nav className="mt-6 w-full">
           <NavItem icon={<Home className="w-5 h-5" />} label="About me" onClickRoutes={onClickRoutes} closeSideBarModal={closeSideBarModal} isActive={activeLink === "About me"} />
-          <NavItem icon={<FaFileCode className="w-5 h-5"/>} label="Portfolio" onClickRoutes={onClickRoutes} closeSideBarModal={closeSideBarModal} isActive={activeLink === "Portfolio"} />
-          <NavItem icon={<MdDesignServices className="w-5 h-5"/>} label="Services" onClickRoutes={onClickRoutes} closeSideBarModal={closeSideBarModal} isActive={activeLink === "Services"} />
-          <NavItem icon={<FaPaperPlane className="w-5 h-5"/>} label="Resume" onClickRoutes={onClickRoutes} closeSideBarModal={closeSideBarModal} isActive={activeLink === "Resume"} />
-          <NavItem icon={<ContactRound className="w-5 h-5"/>} label="Contact" onClickRoutes={onClickRoutes} closeSideBarModal={closeSideBarModal} isActive={activeLink === "Contact"} />
-          {/* <NavItem icon={<FaBlog className="w-5 h-5 ml-1"/>} label="Blog" onClickRoutes={onClickRoutes} closeSideBarModal={closeSideBarModal} isActive={activeLink === "Blog"} /> */}
+          <NavItem icon={<FaFileCode className="w-5 h-5" />} label="Portfolio" onClickRoutes={onClickRoutes} closeSideBarModal={closeSideBarModal} isActive={activeLink === "Portfolio"} />
+          <NavItem icon={<MdDesignServices className="w-5 h-5" />} label="Services" onClickRoutes={onClickRoutes} closeSideBarModal={closeSideBarModal} isActive={activeLink === "Services"} />
+          <NavItem icon={<FaPaperPlane className="w-5 h-5" />} label="Resume" onClickRoutes={onClickRoutes} closeSideBarModal={closeSideBarModal} isActive={activeLink === "Resume"} />
+          <NavItem icon={<ContactRound className="w-5 h-5" />} label="Contact" onClickRoutes={onClickRoutes} closeSideBarModal={closeSideBarModal} isActive={activeLink === "Contact"} />
+          <a target="_blank" href="https://medium.com/@ysachinyadav73" >
+            <NavItem icon={<FaBlog className="w-5 h-5 ml-1" />} label="Blog" />
+          </a>
         </nav>
 
-        <HireMe/>
+        <HireMe />
 
       </div>
     </div>
@@ -68,7 +70,7 @@ const SidebarContent = ({ isSmallScreen, closeSideBarModal }) => {
 
 const NavItem = ({ icon, label, isActive, onClickRoutes, closeSideBarModal }) => (
   <button
-    onClick={() => {onClickRoutes(label); closeSideBarModal()}}
+    onClick={() => { onClickRoutes(label); closeSideBarModal() }}
     className={`flex items-center text-md w-full px-6 cursor-pointer py-3 text-left transition-all duration-200 rounded-md ${isActive ? 'text-accent' : 'hover:bg-[rgb(var(--color-primary))] hover:text-primary'}`}
   >
     {icon}
